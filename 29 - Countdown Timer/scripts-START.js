b4c83d18d895077f2429e09554d3a62630cbd3b3
Dilven <mateusiakkrystian@gmail.com>
2018-05-06 22:16:18 +0200

    complete

---
+++ 29 - Countdown Timer/scripts-START.js
@@ -0,0 +1,49 @@
+const timeLeft = document.querySelector('.display__time-left');
+const endTime = document.querySelector('.display__end-time');
+const timerButtons = document.querySelectorAll('.timer__button');
+
+let countdown;
+
+function timer(seconds) {
+  const milisecToEnd = Date.now() + seconds * 1000;
+  const end = new Date(milisecToEnd)
+  timeToEnd(end);
+  clearInterval(countdown);
+  countdown = setInterval(() => {
+    if (seconds % 60 === 0) seconds - 60;
+    seconds--
+    if (seconds < 0) {
+      clearInterval(countdown)
+      return;
+    }
+    timeToDisplay(seconds)
+  }, 1000)
+};
+
+function timeToDisplay(secs) {
+  const minsToDisplay = Math.floor(secs / 60);
+  const secsToDisplay = secs % 60
+  const timeToDisplay = `${minsToDisplay}:${(secsToDisplay < 10 ? '0' : '')}${secsToDisplay}`
+  timeLeft.textContent = timeToDisplay;
+  document.title = timeToDisplay;
+};
+
+function timeToEnd(end) {
+  const hour = end.getHours();
+  const min = end.getMinutes();
+  const sec = end.getSeconds();
+  endTime.textContent = `You have time to ${hour}:${min < 10 ? '0': ''}${min}:${sec < 10 ? '0': ''}${sec}`;
+};
+
+timerButtons.forEach(timerButton => timerButton.addEventListener('click', function () {
+  const secs = parseInt(this.dataset.time);1
+  timeToDisplay(secs)
+  timer(secs)
+}));
+document.customForm.addEventListener('submit', function(e) {
+  e.preventDefault();
+  const secs = 60 * this.minutes.value;
+  timeToDisplay(secs);
+  timer(secs);
+  this.reset();
+})
